4fae21d5a550ddfec14b49d43169751caf91d85b
Fredrick Stakem <fstakem@gmail.com>
2020-04-16 15:17:34 -0400

    fix typos

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
 STACK_NAME=awsbootstrap
 REGION=us-east-2
 CLI_PROFILE=awsbootstrap
@@ -12,6 +17,21 @@ echo -e "\n\n================| Deploying main.yaml |================"
 aws cloudformation deploy \
     --region $REGION \
     --profile $CLI_PROFILE \
+    --stack-name $STACK_NAME \
+    --template-file main.yml \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --parameter-overrides \
+        EC2InstanceType=$EC2_INSTANCE \
+        GitHubOwner=$GH_OWNER \
+        GitHubRepo=$GH_REPO \
+        GitHubBranch=$GH_BRANCH \
+        GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
+        CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+aws cloudformation deploy \
+    --region $REGION \
+    --profile $CLI_PROFILE \
     --stack-name $STACK_NAME-setup \
     --template-file setup.yml \
     --no-fail-on-empty-changeset \
@@ -19,15 +39,6 @@ aws cloudformation deploy \
     --parameter-overrides \
         CodePipelineBucket=$CODEPIPLINE_BUCKET
 
-aws cloudformation deploy \
-    --region $REGION \
-    --profile $CLI_PROFILE \
-    --stack-name $STACK_NAME \
-    --template-file main.yml \
-    --no-fail-on-empty-changeset \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --parameter-overrides \
-        EC2InstanceType=$EC2_INSTANCE
 
 if [ $? -eq 0 ]; then
     aws cloudformation list-exports \
